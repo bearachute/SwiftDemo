02b70084319d29ee5b5b0165ac6492c6345d9c1f
bearachute <sjourand@gmail.com>
2018-09-03 15:38:22 -0400

    a swift thing

--- SwiftDemo/ViewController.swift
+++ SwiftDemo/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd" // setting date format
+        let result = formatter.string(from: date)
+        self.label.text = result
     }
 
     override func didReceiveMemoryWarning() {
